bc1de7a67be957449dc43102aaa1d1ef76dc6aac
FloChehab <florent.chehab@gmail.com>
2016-11-17 08:12:01 +0100

    Main file

---
+++ main.c
@@ -0,0 +1,45 @@
+#include tp3.h
+
+void main(){
+	T_Magasin *magasin;
+	int state = 1;
+	magasin = testing();
+    while(state){
+        switch(afficherMenu()){
+            case 1:
+            	viderMagasin(magasin);
+                magasin = creerMagasinWrapper();
+                break;
+            case 2:
+            	ajouterRayonWrapper(magasin);
+                break;
+            case 3:
+                ajouterProduitWrapper(magasin);
+                break;
+            case 4:
+                afficherMagasinWrapper(magasin);
+                break;
+            case 5:
+                afficherRayonWrapper(magasin);
+                break;
+            case 6:
+                supprimerProduitWrapper(magasin);
+                break;
+            case 7:
+                supprimerRayonWrapper(magasin);
+                break;
+            case 8:
+                rechercheProduitsWrapper(magasin);
+                break;
+            case 9:
+            	fusionnerRayonsWrapper(magasin);
+            	break;
+            case 10:
+            	viderMagasin(magasin);
+                state = 0;
+                break;
+            default: 
+            	break;
+        }
+    }
+}
